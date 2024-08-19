bb86d67aff5f3a17e079edd97e4f7ce9267f7178
Caleb Nwaizu <calebchris000@gmail.com>
2024-08-19 15:27:07 +0100

    Move ts to init

--- src/init/cursor.ts
+++ src/init/cursor.ts
@@ -3,33 +3,6 @@ cursor.classList.add("cursor");
 
 document.body.appendChild(cursor);
 
-const tooltip = document.createElement("div");
-tooltip.classList.add("tooltip");
-tooltip.textContent = "Right click for options";
-tooltip.innerHTML = `
-  <svg
-      stroke="white"
-      fill="none"
-      width="32"
-      height="32"
-      stroke-width="2"
-      viewBox="0 0 24 24"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      xmlns="http://www.w3.org/2000/svg"
-      ><path d="M7 14h-4v-4"></path><path
-          d="M21 12c-.887 1.284 -2.48 2.033 -4 2c-1.52 .033 -3.113 -.716 -4 -2s-2.48 -2.033 -4 -2c-1.52 -.033 -3 1 -4 2l-2 2"
-      ></path></svg
-  >
-
-  ${tooltip.textContent}
-`;
-document.body.appendChild(tooltip);
-
-if (!localStorage.getItem("tooltip_shown")) {
-  localStorage.setItem("tooltip_shown", JSON.stringify(false));
-}
-
 document.addEventListener("mousedown", (e) => {
   cursor.style.scale = "0.8";
 });
@@ -45,8 +18,6 @@ window.addEventListener("mousemove", (e) => {
   const cursorHeight = cursorRect.height;
   cursor.style.left = `${clientX - cursorWidth / 2}px`;
   cursor.style.top = `${clientY - cursorHeight / 2}px`;
-  tooltip.style.left = `${clientX + 60}px`;
-  tooltip.style.top = `${clientY - 20}px`;
 
   const target = e.target as HTMLElement;
 
@@ -72,18 +43,6 @@ window.addEventListener("mousemove", (e) => {
   const tooltip_shown = JSON.parse(
     localStorage.getItem("tooltip_shown") ?? "0",
   );
-
-  if (typeof tooltip_shown === "boolean" && !tooltip_shown) {
-    setTimeout(() => {
-      tooltip.style.pointerEvents = "auto";
-      tooltip.style.opacity = "1";
-    }, 2000);
-    setTimeout(() => {
-      tooltip.style.pointerEvents = "none";
-      tooltip.style.opacity = "0";
-      localStorage.setItem("tooltip_shown", JSON.stringify(true));
-    }, 6000);
-  }
 });
 
 document.addEventListener("contextmenu", (e) => {
---
+++ src/init/tooltip.ts
@@ -0,0 +1,48 @@
+const tooltip = document.createElement("div");
+tooltip.classList.add("tooltip");
+tooltip.textContent = "Right click for options";
+tooltip.innerHTML = `
+  <svg
+      stroke="white"
+      fill="none"
+      width="32"
+      height="32"
+      stroke-width="2"
+      viewBox="0 0 24 24"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      xmlns="http://www.w3.org/2000/svg"
+      ><path d="M7 14h-4v-4"></path><path
+          d="M21 12c-.887 1.284 -2.48 2.033 -4 2c-1.52 .033 -3.113 -.716 -4 -2s-2.48 -2.033 -4 -2c-1.52 -.033 -3 1 -4 2l-2 2"
+      ></path></svg
+  >
+
+  ${tooltip.textContent}
+`;
+document.body.appendChild(tooltip);
+
+if (!localStorage.getItem("tooltip_shown")) {
+  localStorage.setItem("tooltip_shown", JSON.stringify(false));
+}
+
+window.addEventListener("mousemove", (e) => {
+  const { clientX, clientY } = e;
+  tooltip.style.left = `${clientX + 60}px`;
+  tooltip.style.top = `${clientY - 20}px`;
+
+  const tooltip_shown = JSON.parse(
+    localStorage.getItem("tooltip_shown") ?? "0",
+  );
+
+  if (typeof tooltip_shown === "boolean" && !tooltip_shown) {
+    setTimeout(() => {
+      tooltip.style.pointerEvents = "auto";
+      tooltip.style.opacity = "1";
+    }, 2000);
+    setTimeout(() => {
+      localStorage.setItem("tooltip_shown", JSON.stringify(true));
+      tooltip.style.opacity = "0";
+      tooltip.style.pointerEvents = "none";
+    }, 6000);
+  }
+});
