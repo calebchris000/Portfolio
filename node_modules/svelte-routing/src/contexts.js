7bd9a1eac6b9e00195c31f1702d92f66d5c34826
Caleb Nwaizu <calebchris000@gmail.com>
2024-08-19 13:47:55 +0100

    remove nodemodules

--- node_modules/svelte-routing/src/contexts.js
+++
@@ -1,9 +0,0 @@
-import { getContext } from "svelte";
-
-export const LOCATION = {};
-export const ROUTER = {};
-export const HISTORY = {};
-
-export const useLocation = () => getContext(LOCATION);
-export const useRouter = () => getContext(ROUTER);
-export const useHistory = () => getContext(HISTORY);
--- node_modules/svelte-routing/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="./ambient" />
-
-export { Link } from "./Link";
-export { Route } from "./Route";
-export { Router } from "./Router";
-export { link, links } from "./actions";
-export * from "./contexts";
-export { navigate } from "./functions";
